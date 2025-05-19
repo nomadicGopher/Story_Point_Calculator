6c5095675dad70cd4d7d2c1bac9897412b94043f
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-05-18 17:52:00 -0700

    refactor event listener

--- scripts.js
+++ scripts.js
@@ -3,23 +3,21 @@
 let form = document.forms[0],
   complexity = form.elements["complexity"],
   time = form.elements["time"],
+  fibonacci = form.elements['fibonacci'],
   value;
 
 function calculate() {
   value = time.value * complexity.value;
+
   if (value !== 1) {
     document.getElementById("plural").innerHTML = "s";
   } else {
     document.getElementById("plural").innerHTML = ""
   };
+
   document.getElementById("result").innerHTML = value;
 };
 
-for (let i = 0; i < complexity.length; i++) {
-  complexity[i].addEventListener("change", calculate, false);
-};
-for (let i = 0; i < time.length; i++) {
-  time[i].addEventListener("change", calculate, false);
-};
+form.addEventListener("change", calculate, false)
 
 calculate();
